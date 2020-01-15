cbef26a8c291e02a35cc5ac851e4ba416398a26e
Jakub Głuszecki <jakub.gluszecki@gmail.com>
2020-01-15 16:05:08 +0100

    Add on_disconnect hook for debugging

--- main.py
+++ main.py
@@ -12,6 +12,14 @@ client.username_pw_set(username=os.environ['MQTT_USERNAME'],
 
 client.connect(broker_url, broker_port)
 
+
+def on_disconnect(_client, _userdata, rc):
+    if rc != 0:
+        print("Unexpected MQTT disconnection. Will auto-reconnect")
+
+
 client.on_message = on_message
+client.on_disconnect = on_disconnect
+
 client.subscribe('laseregg')
 client.loop_forever()
