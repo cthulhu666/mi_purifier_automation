191f040181a25475cc1a12eae0f3e5f86f2ceef6
Jakub Głuszecki <jakub.gluszecki@gmail.com>
2020-06-06 16:33:17 +0200

    Do not hardcode broker url/port

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ import paho.mqtt.client as mqtt
 
 from handlers import on_message
 
-broker_url = "farmer.cloudmqtt.com"
-broker_port = 12397
+broker_url = os.environ['MQTT_BROKER_URL']
+broker_port = os.environ['MQTT_BROKER_PORT']
 
 client = mqtt.Client()
 client.username_pw_set(username=os.environ['MQTT_USERNAME'],
                        password=os.environ['MQTT_PASSWORD'])
 
-client.connect(broker_url, broker_port)
+client.connect(broker_url, int(broker_port))
 
 
 def on_disconnect(_client, _userdata, rc):
